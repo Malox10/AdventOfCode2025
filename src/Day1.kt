5d79c32c425fccda0f6f7e69aeea2d97bc02ddaa
Malox <Maxi.Junges@gmx.de>
2025-12-01 13:12:51 +0100

    Day1 Part1

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,11 +1,22 @@
 fun main() {
-    fun parse(input: List<String>) {
-
+    fun parse(input: List<String>): List<Int> {
+        return input.map { line ->
+            val direction = if(line[0] == 'L') -1 else 1
+            val amount = line.substring(1).toInt()
+            direction * amount
+        }
     }
 
     fun part1(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
+        val turns = parse(input)
+        var location = 50
+        var score = 0
+        turns.forEach {
+            location += it
+            location = (location + 100) % 100
+            if(location == 0) score++
+        }
+        return score
     }
 
     fun part2(input: List<String>): Int {
