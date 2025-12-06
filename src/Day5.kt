c4c555bbe9756b88851aa3fe226ec3f50fc8a5a3
Malox <Maxi.Junges@gmx.de>
2025-12-06 01:34:16 +0100

    Day5 Part2

--- src/Day5.kt
+++ src/Day5.kt
@@ -18,16 +18,83 @@ fun main() {
         return unspoiled.count()
     }
 
-    fun part2(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
+    fun combineRanges(ranges: List<LongRange>): List<LongRange> {
+        val remainingRanges = ranges.toMutableList()
+        val currentRanges = emptyList<LongRange>().toMutableList()
+        while(remainingRanges.isNotEmpty()) {
+            val currentRange = remainingRanges.removeFirst()
+            if(currentRanges.isEmpty()) {
+                currentRanges.add(currentRange)
+                continue
+            }
+            val iterator = currentRanges.iterator()
+
+            var didCombine = false
+            while(iterator.hasNext()) {
+                val nextRangeToCheck = iterator.next()
+                //if they are the same
+                if(currentRange.start == nextRangeToCheck.start && currentRange.endInclusive == nextRangeToCheck.endInclusive) {
+                    println(currentRange)
+                    println(nextRangeToCheck)
+                    didCombine = true
+                    break
+                }
+
+                //if new range is completely inside the other range
+                if(currentRange.start in nextRangeToCheck && currentRange.endInclusive in nextRangeToCheck) {
+                    didCombine = true
+                    break
+                }
+
+                //if new range is bigger than other range
+                if(currentRange.start < nextRangeToCheck.start && nextRangeToCheck.endInclusive < currentRange.endInclusive) {
+                    didCombine = true
+                    iterator.remove()
+                    currentRanges.add(currentRange)
+                    break
+                }
+                //if new range is outside other range
+                if(currentRange.start > nextRangeToCheck.endInclusive || currentRange.endInclusive < nextRangeToCheck.start) {
+                    continue
+                }
+                if(currentRange.start in nextRangeToCheck && currentRange.endInclusive > nextRangeToCheck.endInclusive) {
+                    didCombine = true
+                    val newRange = nextRangeToCheck.start..currentRange.endInclusive
+                    iterator.remove()
+                    currentRanges.add(newRange)
+                    break
+                }
+                if(currentRange.endInclusive in nextRangeToCheck && currentRange.start < nextRangeToCheck.start) {
+                    didCombine = true
+                    val newRange = currentRange.start..nextRangeToCheck.endInclusive
+                    iterator.remove()
+                    currentRanges.add(newRange)
+                    break
+                }
+                error("Unreachable")
+            }
+            if(!didCombine) currentRanges.add(currentRange)
+
+        }
+        return currentRanges
     }
 
+    fun part2(input: List<String>): Long {
+        val (ranges, _) = parse(input)
+        var previousSize: Int
+        var rangesToCombine = ranges
+        do {
+            previousSize = rangesToCombine.size
+            val combinedRanges = combineRanges(rangesToCombine)
+            rangesToCombine = combinedRanges
+        } while(previousSize != combinedRanges.size)
 
+        return rangesToCombine.sumOf {  it.endInclusive - it.start + 1 }
+    }
 
     val testInput = readInput("Day5Test")
     checkDebug(part1(testInput), 3)
-//    checkDebug(part2(testInput), 1)
+    checkDebug(part2(testInput), 14)
 
     val input = readInput("Day5")
     "part1: ${part1(input)}".println()
