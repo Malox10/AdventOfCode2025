97a4ca2fab85e3a70200bcd3d67e1abab466ab23
Malox <Maxi.Junges@gmx.de>
2025-12-07 19:10:28 +0100

    Day7 Part2

--- src/Day7.kt
+++ src/Day7.kt
@@ -43,16 +43,58 @@ fun main() {
         return score
     }
 
-    fun part2(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
+
+    //too low 7980502
+    fun part2(input: List<String>, isTest: Boolean = false): Long {
+        val maze = parse(input)
+        var row = 0
+        var columns = mutableMapOf(maze.first().indexOfFirst { it == 'S' } to 1L)
+        while((row + 1) < maze.size) {
+            val beamsToAdd = mutableMapOf<Int, Long>()
+            columns.forEach { (column, count) ->
+                val nextPoint = (row + 1) to column
+                val char = maze[nextPoint]
+                if(char == '.') {
+                    beamsToAdd.addToCountLong(column, count)
+
+//                    maze[(row + 1) to column] = if(isTest) (count % 10).digitToChar() else '|'
+                } else if(char == '^') {
+                    val left = column - 1
+                    val right = column + 1
+                    beamsToAdd.addToCountLong(left, count)
+                    beamsToAdd.addToCountLong(right, count)
+
+                } else if(char == '|' || char?.digitToInt() in 0..9) {
+                    beamsToAdd.addToCountLong(column, count)
+
+//                    maze[(row + 1) to column] = if(isTest) (count % 10).digitToChar() else '|'
+                } else {
+                    error("Unreachable")
+                }
+
+//                maze[(row + 1) to left] = if(isTest) (count % 10).digitToChar() else '|'
+//                maze[(row + 1) to right] = if(isTest) (count % 10).digitToChar() else '|'
+                beamsToAdd.forEach { (column, count) ->
+                    maze[(row + 1) to column] = if(isTest) (count % 10L).toInt().digitToChar() else '|'
+                }
+                columns = beamsToAdd
+            }
+            println(columns.values.sum())
+            maze.forEach { row ->
+                row.joinToString("").println()
+            }
+            println()
+            row++
+        }
+
+        return columns.values.sum()
     }
 
 
 
     val testInput = readInput("Day7Test")
     checkDebug(part1(testInput), 21)
-//    checkDebug(part2(testInput), 1)
+    checkDebug(part2(testInput, true), 40)
 
     val input = readInput("Day7")
     "part1: ${part1(input)}".println()
