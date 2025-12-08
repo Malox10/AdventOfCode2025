f4239e0da0c26065ee78587b28ac39e5744aaee8
Malox <Maxi.Junges@gmx.de>
2025-12-08 16:40:52 +0100

    Day8 Part2

--- src/Day8.kt
+++ src/Day8.kt
@@ -19,34 +19,36 @@ fun main() {
         }
     }
 
-    fun List<DistancePair>.expandNetwork(start: Node): Set<Node> {
-        val nodesToVisit = mutableListOf(start)
+    fun Map<Node, List<Node>>.expandNetwork(start: Node): Set<Node> {
         //prevent double counting of same node in network
+        val nodesToVisit = mutableSetOf(start)
         val visitedNodes = mutableSetOf<Node>()
         while (nodesToVisit.isNotEmpty()) {
-            val nodeToVisit = nodesToVisit.removeFirst()
-            val connectedLinks = this.filter { it.first == nodeToVisit || it.second == nodeToVisit }
-            val connectedNodes = connectedLinks.map { if(it.first == nodeToVisit) it.second else it.first }
-                .filter { !visitedNodes.contains(it) }
-            nodesToVisit.addAll(connectedNodes)
+            val nodeToVisit = nodesToVisit.first()
+            nodesToVisit.remove(nodeToVisit)
+            val connectedNodes = this[nodeToVisit] ?: emptyList()
+            nodesToVisit.addAll(connectedNodes.filter { !visitedNodes.contains(it) })
             visitedNodes.add(nodeToVisit)
         }
         return visitedNodes
     }
-    fun List<DistancePair>.findAllNetworks(): List<Set<Node>> {
-        val nodesToExplore = this.flatMap { listOf(it.first, it.second) }.toMutableSet()
+    fun List<DistancePair>.findAllNetworks(nodesToExplore: Set<Node>? = null): List<Set<Node>> {
+        val nodesToExplore = nodesToExplore?.toMutableSet() ?: this.flatMap { listOf(it.first, it.second) }.toMutableSet()
+        val nodeMap = mutableMapOf<Node, MutableList<Node>>()
+        this.forEach { pair ->
+            nodeMap.addToList(pair.first, pair.second)
+            nodeMap.addToList(pair.second, pair.first)
+        }
         val networks = mutableListOf<Set<Node>>()
         while(nodesToExplore.isNotEmpty()) {
             val nodeToExplore = nodesToExplore.first()
-            val network = expandNetwork(nodeToExplore)
+            val network = nodeMap.expandNetwork(nodeToExplore)
             nodesToExplore.removeAll(network)
             networks.add(network)
         }
         return networks
     }
 
-
-    //14814 too low
     fun part1(input: List<String>, numberOfConnections: Int = 1000): Int {
         val nodes = parse(input)
 
@@ -69,16 +71,58 @@ fun main() {
         return sizes.reduce(Int::times)
     }
 
-    fun part2(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
+    //479575008 too low
+    fun part2(input: List<String>, startNumberOfConnections: Int = 10): Long {
+        val nodes = parse(input)
+
+        val distances = mutableListOf<DistancePair>()
+        nodes.forEachIndexed { index, node ->
+            ((index + 1) until nodes.size).forEach { secondNodeIndex ->
+                val secondNode = nodes[secondNodeIndex]
+                if(node == secondNode) return@forEach
+                val distance = node.distanceTo(secondNode)
+                distances.add(DistancePair(node, secondNode, distance))
+            }
+        }
+
+        distances.sortBy { it.distance }
+
+        var lowestWorkingSolution = distances.size
+        var highestFail = 0
+        var currentNumberOfLinks = startNumberOfConnections
+
+        val mutableNodes =  nodes.toSet()
+        while(true) {
+            val currentLinks = distances.take(currentNumberOfLinks)
+            val networks = currentLinks.findAllNetworks(mutableNodes)
+            val isOneNetwork = networks.size == 1
+            if(isOneNetwork) {
+                lowestWorkingSolution = currentNumberOfLinks
+                currentNumberOfLinks = (lowestWorkingSolution + highestFail) / 2
+            } else {
+                highestFail = currentNumberOfLinks
+                if(highestFail < (lowestWorkingSolution / 2)) {
+                    currentNumberOfLinks *= 2
+                } else {
+                    currentNumberOfLinks = (lowestWorkingSolution + highestFail) / 2
+                }
+            }
+
+            println("lowestWorkingSolution: $lowestWorkingSolution")
+            println("currentNumberOfLinks: $currentNumberOfLinks")
+            println("highestFail: $highestFail")
+            if(lowestWorkingSolution - highestFail == 1) {
+                val winningLink = distances[lowestWorkingSolution - 1]
+                return winningLink.first.x.toLong() * winningLink.second.x
+            }
+        }
     }
 
 
 
     val testInput = readInput("Day8Test")
     checkDebug(part1(testInput, 10), 40)
-//    checkDebug(part2(testInput), 1)
+    checkDebug(part2(testInput), 25272)
 
     val input = readInput("Day8")
     "part1: ${part1(input)}".println()
